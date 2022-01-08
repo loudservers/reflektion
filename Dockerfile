5d08a0f528c87abf5f953ebb17a06319954de33a
Marcus Gutierrez <marcusgttrz@gmail.com>
2022-01-08 13:50:42 -0600

    It commits!

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,15 @@ RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2
     && unzip awscliv2.zip \
     && ./aws/install
 
+RUN mkdir -pv ~/.ssh
+RUN echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts
+
 # Copy custom runtime bootstrap
 COPY bootstrap ${LAMBDA_RUNTIME_DIR}
+RUN chmod +x ${LAMBDA_RUNTIME_DIR}/bootstrap
 
 # Copy function code
 COPY function.sh ${LAMBDA_TASK_ROOT}
-
-RUN chmod +x ${LAMBDA_RUNTIME_DIR}/bootstrap
 RUN chmod +x ${LAMBDA_TASK_ROOT}/function.sh
 
 # Set the CMD to your handler (could also be done as a parameter override outside of the Dockerfile)
--- function.sh
+++ function.sh
@@ -1,19 +1,75 @@
 #!/bin/sh
 
+export GIT_SSH="/tmp/.ssh"
+export GIT_ID_RSA="${GIT_SSH}/id_rsa"
+export GIT_SSH_COMMAND="ssh -i ${GIT_ID_RSA}" # Automatically points git to use this command
+
+console () {
+    echo "$1" 1>&2
+}
+
 handler () {
     EVENT_DATA=$1
-    echo "$EVENT_DATA" 1>&2
+    console "$EVENT_DATA"
+    prepare_key
 
-    repo=$(echo $EVENT_DATA | jq .repo)
-    image_name=$(echo $EVENT_DATA | jq .repo_name)
+    image_name=$(echo $EVENT_DATA | jq -r .repo_name)
+    image_tag=$(echo $EVENT_DATA | jq -r .image_tag)
+    app_name=$(echo $EVENT_DATA | jq -r .app)
 
-    git clone --single-branch git@github.com:mgtrrz/bookfeed.git "/tmp/${repo}"
+    prepare_git # sets dest_repo_dir
+    console "Dir: $dest_repo_dir"
 
+    app_file="$dest_repo_dir/${APPS_DIR}/$app_name.yaml"
+    console "App: $app_file"
+    if [ ! -f "$app_file" ]; then
+        console "App file does not exist!"
+        exit 1
+    fi
 
-    RESPONSE="Echoing json_data: '$json_data'"
+    sed -i "s/${IMAGE_TAG_VAR}: \".*\"/${IMAGE_TAG_VAR}: \"$image_tag\"/g" "$app_file"
+
+    commit_change "$app_name"
+
+    RESPONSE="Complete"
     echo $RESPONSE
 }
 
 prepare_key () {
-    
+    console "Preparing key.."
+    echo "$SSH_KEY_PARAMETER" 1>&2
+    mkdir -pv $GIT_SSH
+
+    aws ssm get-parameter --name "${SSH_KEY_PARAMETER}" --with-decryption --query 'Parameter.Value' --output text > ${GIT_ID_RSA}
+    chmod 400 ${GIT_ID_RSA}
+}
+
+prepare_git () {
+    console "Preparing git.."
+    local dest_repo_name=$(echo "$DESTINATION_REPO" | cut -d'/' -f2 | cut -d'.' -f1)
+
+    dest_repo_dir="/tmp/$dest_repo_name"
+
+    git clone --single-branch --branch "${BRANCH}" "$DESTINATION_REPO" "$dest_repo_dir"
+
+    echo "[user]
+	    email = reflektion@loudservers.com
+	    name = reflektion" >> "$dest_repo_dir/.git/config"
+}
+
+commit_change () {
+    console "Commiting change.."
+    local app_name=$1
+    cd "$dest_repo_dir"
+    git add -A
+    if [ -n "$(git status --porcelain)" ]; then
+        git commit -m "Updating image tag for $app_name"
+	    git push origin HEAD
+    else
+        echo "No changes to make"
+    fi
+}
+
+cleanup () {
+    rm -rf /tmp
 }
